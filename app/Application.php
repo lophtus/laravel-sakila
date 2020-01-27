09dc668839753e6e36d6b85b121414aa07dfb1f9
Brett Ethridge <brett@brettethridge.com>
2020-01-27 17:09:57 -0600

    Added openapi annotation deps and swagger-ui

---
+++ app/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Application as BaseApplication;
+
+/**
+ * @OA\Info(
+ *  title="Api",
+ *  description="An api for accessing the Sakila system",
+ *  version="0.1",
+ * )
+ */
+class Application extends BaseApplication
+{
+}
