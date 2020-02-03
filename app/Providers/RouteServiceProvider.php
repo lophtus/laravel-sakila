2913ef89a7d084ce67d5b233344cb3ff010ed86d
Brett Ethridge <brett@brettethridge.com>
2020-02-03 13:02:51 -0600

    Moved film api routes to dedicated route file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -75,6 +75,8 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
              ->middleware('api')
              ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+             ->group(function ($router) {
+                 require base_path('routes/api/films.php');
+             });
     }
 }
