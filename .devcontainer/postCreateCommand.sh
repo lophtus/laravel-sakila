95fac816b06f87e3e630f752c0d2729b4226edc9
Brett Ethridge <brett@brettethridge.com>
2025-01-08 03:36:13 +0000

    Fix node install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+NODE_VERSION=16
+
 # Setup and enable apache2 site
 sudo cp ./.devcontainer/etc/apache2/sites-available/laravel-sakila.conf /etc/apache2/sites-available/laravel-sakila.conf
 sudo a2ensite laravel-sakila
@@ -11,4 +13,4 @@ sudo a2enmod rewrite
 sudo service apache2 restart
 
 # Install Node.js
-nvm install 16
+. /usr/local/share/nvm/nvm.sh && nvm install ${NODE_VERSION} 2>&1
