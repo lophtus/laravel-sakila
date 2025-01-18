bdc1cb12fee504f4138277a9537fe7c61e3fdce6
lophtus <73408219+lophtus@users.noreply.github.com>
2025-01-18 09:47:15 -0600

    Add axios loading indicator (#79)

--- resources/js/admin/bootstrap.js
+++ resources/js/admin/bootstrap.js
@@ -1,15 +1,14 @@
-window._ = require('lodash');
-
 /**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * We'll load  which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
  */
 
 try {
-    window.Popper = require('popper.js').default;
+  window._ = require('lodash');
+  window.Popper = require('popper.js').default;
 
-    require('bootstrap');
+  require('bootstrap');
 } catch (e) { }
 
 /**
