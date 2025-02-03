db98438c36bebf6530f71657d339bfa0a15ff8c7
Brett Ethridge <brett@brettethridge.com>
2025-02-03 02:17:35 +0000

    Fix toast position

--- resources/js/admin/composables/useToast.ts
+++ resources/js/admin/composables/useToast.ts
@@ -3,10 +3,14 @@ import { useToasted } from "@hoppscotch/vue-toasted";
 export default function useToast() {
   const toasted = useToasted();
 
+  const position = 'bottom-right';
+  const duration = 2000;
+
   function toastSuccess (message: string) {
     return toasted.show(message,
       {
-        duration: 2000,
+        position,
+        duration,
         type: "success",
         icon: "icon cil-check-circle me-2",
       }
@@ -16,7 +20,8 @@ export default function useToast() {
   function toastError (message: string) {
     return toasted.show(message,
       {
-        duration: 2000,
+        position,
+        duration,
         type: "error",
         icon: "icon cil-x-circle me-2",
       }
